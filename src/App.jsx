caac276bc0057c54127fedde4f8ffe9f87b92a86
Julien Heng <julienheng0@gmail.com>
2024-02-06 15:27:32 +0100

    Setting up home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import Card from "./components/Card"
+import Card from "./components/Card";
+import './assets/css/style.css';
 
 
 function App() {
-  return <Card word={'Rose'} wordTransled={'Pink'} image={'https://img.freepik.com/photos-gratuite/flou-abstrait-fond-ton-chaud-pastel-belle-couleur-rose-peche-ciel-pour-conception-tant-que-bannerslid_1258-88720.jpg'} />
+  return (
+    <>
+    <header>
+      <h1>Flashcards</h1>
+      <div className="header-buttons">
+        <a href="#">Connexion</a>
+        <a href="#">Inscription</a>
+      </div>
+    </header>
+    <main>
+      <section className="flashcards-container">
+       <Card word={'Rose'} wordTransled={'Pink'} image={'https://img.freepik.com/photos-gratuite/flou-abstrait-fond-ton-chaud-pastel-belle-couleur-rose-peche-ciel-pour-conception-tant-que-bannerslid_1258-88720.jpg'} />
+        </section>
+      <button className="add-flashcard-button">
+        <i className="fas fa-plus"></i>
+        Créer une carte
+      </button>
+    </main>
+    </>
+  )
 }
 
 export default App
