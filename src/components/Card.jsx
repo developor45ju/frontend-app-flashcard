6f1b2c55db3e6384736d9bf98d68794f08e8d0a8
Julien Heng <julienheng0@gmail.com>
2024-02-06 14:54:27 +0100

    Add preprosesor SCSS and autoprefixer

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import '../assets/css/style.css';
+
+const Card = ( props ) => {
+    const word = props.word;
+    const wordTransled = props.wordTransled;
+    const image = props.image;
+
+    return (
+        <div className="flashcard">
+            <div className="front">
+            <h1 className="word">{word}</h1>
+            <img src={image} alt="Image" />
+            </div>
+            <div className="back">
+            <p className="translation">{wordTransled}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
